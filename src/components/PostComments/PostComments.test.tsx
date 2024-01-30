5cd7ee0563058cd09d35ded3d7f610257c4b9409
Artur Matos <arturrtutu@gmail.com>
2024-01-30 16:45:11 -0300

    adicionando testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import Post from './index';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe('Teste para o componente Post', () => {
+    it('Deve adicionar um comentário corretamente', () => {
+        render(<Post />);
+        
+        fireEvent.change(screen.getByRole('textbox'), { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(screen.getByText('Comentar'));
+
+ 
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<Post />);
+        
+
+        fireEvent.change(screen.getByRole('textbox'), { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(screen.getByText('Comentar'));
+
+
+        fireEvent.change(screen.getByRole('textbox'), { target: { value: 'Segundo comentário' } });
+        fireEvent.click(screen.getByText('Comentar'));
+
+
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
+
